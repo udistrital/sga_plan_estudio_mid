d3dc828834e4ec14418f16e8ee5741a6d7c2c978
Stiven Osorio <jsosoriov@udistrital.edu.co>
2026-02-09 09:41:18 -0500

    Modificaciones necesarias para implementación de lineamientos
    Modificaciones al realizar cambio de nombre de proyecto, modificaciones dadas por **go fmt ./...** y **go vet ./...***.

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -7,40 +7,40 @@ import (
 
 func init() {
 
-    beego.GlobalControllerRouter["github.com/udistrital/sga_plan_estudio_mid/controllers:PlanEstudiosController"] = append(beego.GlobalControllerRouter["github.com/udistrital/sga_plan_estudio_mid/controllers:PlanEstudiosController"],
-        beego.ControllerComments{
-            Method: "PostBaseStudyPlan",
-            Router: "/",
-            AllowHTTPMethods: []string{"post"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
+	beego.GlobalControllerRouter["github.com/udistrital/sga_plan_estudio_mid/controllers:PlanEstudiosController"] = append(beego.GlobalControllerRouter["github.com/udistrital/sga_plan_estudio_mid/controllers:PlanEstudiosController"],
+		beego.ControllerComments{
+			Method:           "PostBaseStudyPlan",
+			Router:           "/",
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
-    beego.GlobalControllerRouter["github.com/udistrital/sga_plan_estudio_mid/controllers:PlanEstudiosController"] = append(beego.GlobalControllerRouter["github.com/udistrital/sga_plan_estudio_mid/controllers:PlanEstudiosController"],
-        beego.ControllerComments{
-            Method: "GetStudyPlanVisualization",
-            Router: "/:plan_id/estructura-visualizacion",
-            AllowHTTPMethods: []string{"get"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
+	beego.GlobalControllerRouter["github.com/udistrital/sga_plan_estudio_mid/controllers:PlanEstudiosController"] = append(beego.GlobalControllerRouter["github.com/udistrital/sga_plan_estudio_mid/controllers:PlanEstudiosController"],
+		beego.ControllerComments{
+			Method:           "GetStudyPlanVisualization",
+			Router:           "/:plan_id/estructura-visualizacion",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
-    beego.GlobalControllerRouter["github.com/udistrital/sga_plan_estudio_mid/controllers:PlanEstudiosController"] = append(beego.GlobalControllerRouter["github.com/udistrital/sga_plan_estudio_mid/controllers:PlanEstudiosController"],
-        beego.ControllerComments{
-            Method: "GetPlanPorDependenciaVinculacionTercero",
-            Router: "/dependencias-vinculacion-terceros/:tercero_id",
-            AllowHTTPMethods: []string{"get"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
+	beego.GlobalControllerRouter["github.com/udistrital/sga_plan_estudio_mid/controllers:PlanEstudiosController"] = append(beego.GlobalControllerRouter["github.com/udistrital/sga_plan_estudio_mid/controllers:PlanEstudiosController"],
+		beego.ControllerComments{
+			Method:           "GetPlanPorDependenciaVinculacionTercero",
+			Router:           "/dependencias-vinculacion-terceros/:tercero_id",
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
-    beego.GlobalControllerRouter["github.com/udistrital/sga_plan_estudio_mid/controllers:PlanEstudiosController"] = append(beego.GlobalControllerRouter["github.com/udistrital/sga_plan_estudio_mid/controllers:PlanEstudiosController"],
-        beego.ControllerComments{
-            Method: "PostGenerarDocumentoPlanEstudio",
-            Router: "/generador-documentos-malla",
-            AllowHTTPMethods: []string{"post"},
-            MethodParams: param.Make(),
-            Filters: nil,
-            Params: nil})
+	beego.GlobalControllerRouter["github.com/udistrital/sga_plan_estudio_mid/controllers:PlanEstudiosController"] = append(beego.GlobalControllerRouter["github.com/udistrital/sga_plan_estudio_mid/controllers:PlanEstudiosController"],
+		beego.ControllerComments{
+			Method:           "PostGenerarDocumentoPlanEstudio",
+			Router:           "/generador-documentos-malla",
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
 
 }
